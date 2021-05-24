53aecc65a8ebf337fabbe3edc56fa4cf2242f6f4
Jonathan Morgan <jonathan.morgan.computer@gmail.com>
2021-05-24 12:25:25 -0600

    First commit
    First commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D playerRigidBody;
+    private Vector2 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector2.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+
+        playerRigidBody.velocity = change * speed;
+    }
+
+
+}
