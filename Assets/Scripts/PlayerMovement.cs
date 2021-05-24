60f7da842e7de7c5e5ca0a706412706a2506b7fc
Jonathan Morgan <jonathan.morgan.computer@gmail.com>
2021-05-24 14:09:51 -0600

    Walking animation fluid
    Walkting animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D playerRigidBody;
     private Vector2 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,19 @@ public class PlayerMovement : MonoBehaviour
         change = Vector2.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+
         playerRigidBody.velocity = change * speed;
+        if (change != Vector2.zero)
+        {
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("Moving", true);
+
+        }else
+        {
+            animator.SetBool("Moving", false);
+        }
+
     }
 
 
